d44225e8a1034898ee98b6e4d464d90b56847e26
Ana Miloradovic <ana.miloradovic7@gmail.com>
2022-09-26 12:53:47 +0200

    Initial Commit

---
+++ VicertHIS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VicertHIS
+//
+//  Created by Ana Miloradovic on 26.9.22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
