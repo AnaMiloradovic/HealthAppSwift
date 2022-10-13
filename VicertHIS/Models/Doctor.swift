f65891b54c4615f8a4845903b925c207058fd12a
Ana Miloradovic <ana.miloradovic7@gmail.com>
2022-10-13 14:15:43 +0200

    Delete some unnecessary lines

--- VicertHIS/Models/Doctor.swift
+++ VicertHIS/Models/Doctor.swift
@@ -33,26 +33,6 @@ struct DoctorsResults: Codable {
     let address, phone, dateOfBirth: String?
 }
 
-// MARK: - Helper functions for creating encoders and decoders
-
-
-// MARK: - URLSession response handlers
-
-extension URLSession {
-    fileprivate func codableTask<T: Codable>(with url: URL, completionHandler: @escaping (T?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-        return self.dataTask(with: url) { data, response, error in
-            guard let data = data, error == nil else {
-                completionHandler(nil, response, error)
-                return
-            }
-            completionHandler(try? newJSONDecoder().decode(T.self, from: data), response, nil)
-        }
-    }
-
-    func getAllDoctorsTask(with url: URL, completionHandler: @escaping (GetAllDoctors?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-        return self.codableTask(with: url, completionHandler: completionHandler)
-    }
-}
 
 
 
